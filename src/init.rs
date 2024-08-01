8146f8ac3f0e96d0ff55826c59b64ebb751e7d13
Spencer Kohan <spencerkohan@gmail.com>
2024-08-01 21:36:27 +0200

    switch to toml from json

--- src/init.rs
+++ src/init.rs
@@ -10,8 +10,8 @@ use std::io::prelude::*;
 pub enum InitError {
     #[error("Config file error: {0}")]
     Io(std::io::Error),
-    #[error("JSON error: {0}")]
-    Serde(serde_json::error::Error),
+    #[error("TOML error: {0}")]
+    Toml(String),
 }
 
 fn create_dirsync_dirs() -> Result<(), std::io::Error> {
@@ -24,9 +24,9 @@ pub fn init_dirsync_dir(remote_options: RemoteConfigRecord) -> Result<(), InitEr
     create_dirsync_dirs().map_err(|err| InitError::Io(err))?;
     let _ignore_file = File::create("./.dirsync/ignore").map_err(|err| InitError::Io(err))?;
     let mut config_file =
-        File::create("./.dirsync/config.json").map_err(|err| InitError::Io(err))?;
+        File::create("./.dirsync/config.toml").map_err(|err| InitError::Io(err))?;
     let config = Config::new(remote_options);
-    let json = serde_json::to_string_pretty(&config).map_err(|err| InitError::Serde(err))?;
+    let json = toml::to_string_pretty(&config).map_err(|err| InitError::Toml(err.to_string()))?;
     config_file
         .write_all(json.as_bytes())
         .map_err(|err| InitError::Io(err))?;
